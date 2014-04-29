c0303c78d79dfc92209c79194e2e18fd3ebdab8e
Dennis Monsewicz <dmonsewicz@myemma.com>
2014-04-28 22:23:37 -0500

    working on implementing wysihtml5 editor

--- client/views/admin/posts/posts.js
+++ client/views/admin/posts/posts.js
@@ -1,3 +1,16 @@
+var initEditor = function() {
+  var txtArea = $('textarea').wysihtml5();
+  txtArea.data("wysihtml5").editor.setValue($('textarea').val());
+}
+
 Template.post.rendered = function(){
   return this.$('span.time').timeago();
+}
+
+Template.postEdit.rendered = function() {
+  initEditor();
+}
+
+Template.postNew.rendered = function() {
+  initEditor();
 }
