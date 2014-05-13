fe935e12c9195ba8e7732d59a829be3be5294496
Dennis Monsewicz <dmonsewicz@myemma.com>
2014-05-13 15:45:52 -0500

    adding in settings

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ mkdir meteor-cms-bootstrap-3
 git clone git@github.com:dennismonsewicz/meteor-cms-bootstrap-3.git meteor-cms-bootstrap-3
 cd meteor-cms-bootstrap-3
 mrt install
-meteor
+meteor --settings settings.json
 ```
 
 First-Time Login
@@ -43,6 +43,11 @@ Front-end Template
 Currently, the front-end template that is in place is http://www.blacktie.co/demo/instant/index.html
 
 Feel free to rip this out and add your own! In the long-term 10,000 foot view, there is no plan to implement Templating, but I could be persuaded :)
+
+Settings.json
+-------------
+There are a few front-end helpers that are used to populate (see `client/helpers/app_helpers.js`) a few variables in the footer.
+To modify these variables, please replace the placeholder text in `settings.json` with your own
 
 
 Contributing
--- client/helpers/app_helpers.js
+++ client/helpers/app_helpers.js
@@ -3,8 +3,29 @@ UI.registerHelper('toggleActive', function(route){
     var pathName = Router.current().route.name;
     return (pathName === route) ? 'active' : '';
   }
-})
+});
 
 UI.registerHelper('tagLine', function(){
   return 'FATHER, HUSBAND, WEB DEVELOPER';
-})
+});
+
+UI.registerHelper('footerTagLine', function(){
+  if(_.has(Meteor.settings.public, 'footer_tagline'))
+    return Meteor.settings.public.footer_tagline;
+  else
+    return "";
+});
+
+UI.registerHelper('siteContactEmail', function(){
+  if(_.has(Meteor.settings.public, 'site_contact_email'))
+    return Meteor.settings.public.site_contact_email;
+  else
+    return "foo@baz.com";
+});
+
+UI.registerHelper('footerCityState', function(){
+  if(_.has(Meteor.settings.public, 'footer_city_state'))
+    return Meteor.settings.public.footer_city_state;
+  else
+    return "Foo, Qux";
+});
